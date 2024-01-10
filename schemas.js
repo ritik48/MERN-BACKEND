5639716d5b6af9ac8c155dc09d15935791385ae3
Ritik Raj <84488726+ritik48@users.noreply.github.com>
2024-01-10 23:32:05 +0530

    Update schemas.js

--- schemas.js
+++ schemas.js
@@ -1,13 +1,13 @@
 const Joi = require("joi");
 
 const userSchema = Joi.object({
-    name: Joi.string().min(4).max(10).required(),
+    name: Joi.string().min(3).max(10).required(),
     password: Joi.string().min(3).required(),
     email: Joi.string().email({
         minDomainSegments: 2,
         tlds: { allow: ["com", "net"] },
     }),
-    phone: Joi.number().min(10).max(10),
+    phone: Joi.number().min(6000000000).max(9999999999),
 });
 
 module.exports = userSchema;
